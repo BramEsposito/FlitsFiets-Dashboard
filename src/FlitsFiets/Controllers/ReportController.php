404060ab43883677bd660a35873ac1f013827c38
Bram Esposito <bram@bramesposito.com>
2019-10-04 19:28:41 +0200

    Clean up

--- src/FlitsFiets/Controllers/ReportController.php
+++ src/FlitsFiets/Controllers/ReportController.php
@@ -4,10 +4,12 @@ namespace FlitsFiets\Controllers;
 use FlitsFiets\Models\ReportModel;
 
 class ReportController {
+  private $m; # model
 
   public function __construct() {
     $this->m = new ReportModel();
   }
+
   public function getIntervalData() {
     return $this->m->getIntervalData();
   }
